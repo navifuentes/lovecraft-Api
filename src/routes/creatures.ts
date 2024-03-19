1dc2148fdc4ba87bd2b76f84d40bf2352142e52f
Ivan Fuentes <iv.ifa94@gmail.com>
2024-03-19 13:12:53 +0100

    fixed creatures sorted and indexes

--- src/routes/creatures.ts
+++ src/routes/creatures.ts
@@ -1,10 +1,26 @@
 import express from "express";
 import * as creaturesServices from "../services/creatures";
+import { Creature } from "../types";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  return res.send(creaturesServices.getCreatures());
+  res.json(creaturesServices.getCreatures());
+});
+
+router.get("/index", (_req, res) => {
+  const result = creaturesServices.getCreatures();
+  const reward = result.map((c: Creature, index: number) => ({
+    ...c,
+    id: index + 1,
+  }));
+  res.json(reward);
+});
+
+router.get("/undefined", (_req, res) => {
+  const result = creaturesServices.getCreatures();
+  const reward = result.filter((c) => c.overview === "undefined");
+  res.json(reward);
 });
 
 router.get("/:id", (req, res) => {
