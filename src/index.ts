d1d803746fac1b46b47902f5fe8241a15848c9a4
Ivan Fuentes <iv.ifa94@gmail.com>
2024-03-21 19:37:32 +0100

    test img deploys

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-import helmet from "helmet";
 
 import creaturesRouter from "./routes/creatures";
 import racesRouter from "./routes/races";
@@ -9,20 +8,18 @@ import categoriesRouter from "./routes/classes";
 
 const app = express();
 
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+app.use(express.json());
 app.use(express.static("src"));
 
 const PORT = 3000;
+
+app.use("/api/images", express.static(path.join(__dirname, "images")));
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use("/api/images", express.static(path.join(__dirname, "images")));
 app.use("/api/creatures", creaturesRouter);
 app.use("/api/races", racesRouter);
 app.use("/api/categories", categoriesRouter);
